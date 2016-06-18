a958e34d8a754c65ab3b6d0c9fdd56dcd564493c
infernalslam <you@example.com>
2016-06-19 02:15:28 +0700

    add

--- src/components/register.vue
+++ src/components/register.vue
@@ -1,25 +1,15 @@
 <template>
   {{msg}} <br>
-<mt-field label="username" placeholder="Input username"></mt-field>
-<mt-field label="email" placeholder="Input email" type="email"></mt-field>
-<mt-field label="password" placeholder="Input password" type="password"></mt-field>
-<mt-field label="phone" placeholder="Input tel" type="tel"></mt-field>
-<mt-field label="website" placeholder="Input website" type="url"></mt-field>
-<mt-field label="number" placeholder="Input number" type="number"></mt-field>
-<mt-field label="birthday" placeholder="Input birthday" type="date"></mt-field>
-<!-- <mt-field label="introduction" placeholder="introduction" type="textarea" rows="4"></mt-field>
-<mt-field label="email" state="success"></mt-field>
-<mt-field label="email" state="error"></mt-field>
-<mt-field label="email" state="warning"></mt-field> -->
+  <a class="active item" v-link="{ path: '/Disable' }">คนพิการ</a>
+  <a class="item" v-link="{ path: '/Runner' }">อาสาสมัคร</a>
 </template>
 <script>
-import Field from 'mint-ui'
-import 'mint-ui/lib/cell/style.css'
+// import Field from 'mint-ui'
+// import 'mint-ui/lib/cell/style.css'
 export default {
   data () {
     return {
-      msg: 'Register page',
-      components: Field
+      msg: 'สมัครสมาชิก'
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Mint from 'mint-ui'
+// import Mint from 'mint-ui'
 import Index from './components/index'
 import Register from './components/register'
 import Check from './components/check'
-Vue.use(Mint)
+// Vue.use(Mint)
 Vue.use(VueRouter)
 Vue.use(require('vue-resource'))
 var App = Vue.extend({})
